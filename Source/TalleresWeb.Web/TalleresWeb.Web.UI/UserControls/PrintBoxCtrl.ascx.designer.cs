c1dcd4893352fa7d3d2904e227f64fa87468730f
eosSistemas <muller_federico@hotmail.com>
2025-07-27 15:42:52 -0300

    comit inicial PEAR

--- Source/TalleresWeb.Web/TalleresWeb.Web.UI/UserControls/PrintBoxCtrl.ascx.designer.cs
+++ Source/TalleresWeb.Web/TalleresWeb.Web.UI/UserControls/PrintBoxCtrl.ascx.designer.cs
@@ -55,7 +55,7 @@ namespace TalleresWeb.Web.UI.UserControls {
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlGenericControl frmReporte;
+        protected global::System.Web.UI.HtmlControls.HtmlIframe frmReporte;
         
         /// <summary>
         /// imgAccesoDenegado control.
