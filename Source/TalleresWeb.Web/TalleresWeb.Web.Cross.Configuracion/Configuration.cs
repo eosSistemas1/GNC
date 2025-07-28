47a573cb69172aaba9a3d2c2f05a0f39148dc428
eosSistemas <muller_federico@hotmail.com>
2025-07-28 13:27:52 -0300

    fix page name

--- Source/TalleresWeb.Web/TalleresWeb.Web.Cross.Configuracion/Configuration.cs
+++ Source/TalleresWeb.Web/TalleresWeb.Web.Cross.Configuracion/Configuration.cs
@@ -6,22 +6,22 @@ namespace CrossCutting.DatosDiscretos
     {
         public static String LogoEmpresa = System.Web.Configuration.WebConfigurationManager.AppSettings["LOGOEMPRESA"].ToString();
         public static String RazonSocialEmpresa = System.Web.Configuration.WebConfigurationManager.AppSettings["RAZONSOCIALEMPRESA"].ToString();        
-        public static String DomicilioEmpresa = System.Web.Configuration.WebConfigurationManager.AppSettings["DOMICILIOEMPRESA"].ToString();
-        public static String LocalidadEmpresa = System.Web.Configuration.WebConfigurationManager.AppSettings["LOCALIDADEMPRESA"].ToString();
+        public static String DomicilioEmpresa = System.Web.Configuration.WebConfigurationManager.AppSettings["DOMICILIOEMPRESA"]?.ToString();
+        public static String LocalidadEmpresa = System.Web.Configuration.WebConfigurationManager.AppSettings["LOCALIDADEMPRESA"]?.ToString();
         public static String CUITEmpresa = System.Web.Configuration.WebConfigurationManager.AppSettings["CUITEMPRESA"].ToString();
-        public static String IIBBEmpresa = System.Web.Configuration.WebConfigurationManager.AppSettings["IIBBEMPRESA"].ToString();
-        public static String IVAEmpresa = System.Web.Configuration.WebConfigurationManager.AppSettings["IVAEMPRESA"].ToString();
-        public static String NroAgRetencionIIBB = System.Web.Configuration.WebConfigurationManager.AppSettings["NROAGRETENCIONIIBB"].ToString();
+        public static String IIBBEmpresa = System.Web.Configuration.WebConfigurationManager.AppSettings["IIBBEMPRESA"]?.ToString();
+        public static String IVAEmpresa = System.Web.Configuration.WebConfigurationManager.AppSettings["IVAEMPRESA"]?.ToString();
+        public static String NroAgRetencionIIBB = System.Web.Configuration.WebConfigurationManager.AppSettings["NROAGRETENCIONIIBB"]?.ToString();
 
         public static String FormatoNumerico2d = System.Web.Configuration.WebConfigurationManager.AppSettings["FORMATONUMERICO2D"].ToString();
-        public static String FormatoNumerico4d = System.Web.Configuration.WebConfigurationManager.AppSettings["FORMATONUMERICO"].ToString();
+        public static String FormatoNumerico4d = System.Web.Configuration.WebConfigurationManager.AppSettings["FORMATONUMERICO"]?.ToString();
         public static int CantCopiasFacturaVenta = System.Web.Configuration.WebConfigurationManager.AppSettings["COPIASFACTURAVENTA"] != null ? int.Parse(System.Web.Configuration.WebConfigurationManager.AppSettings["COPIASFACTURAVENTA"].ToString()) : 1;
         public static int CantCopiasRemitoVenta = System.Web.Configuration.WebConfigurationManager.AppSettings["COPIASREMITOVENTA"] != null ? int.Parse(System.Web.Configuration.WebConfigurationManager.AppSettings["COPIASREMITOVENTA"].ToString()) : 1;
         public static int CantCopiasPago = System.Web.Configuration.WebConfigurationManager.AppSettings["COPIASPAGO"] != null ? int.Parse(System.Web.Configuration.WebConfigurationManager.AppSettings["COPIASPAGO"].ToString()) : 1;
         public static int CantCopiasCertifRetIIBB = System.Web.Configuration.WebConfigurationManager.AppSettings["COPIASCERTIFIIBB"] != null ? int.Parse(System.Web.Configuration.WebConfigurationManager.AppSettings["COPIASCERTIFIIBB"].ToString()) : 1;
         public static int CantCopiasCobranza = System.Web.Configuration.WebConfigurationManager.AppSettings["COPIASCOBRANZA"] != null ? int.Parse(System.Web.Configuration.WebConfigurationManager.AppSettings["COPIASCOBRANZA"].ToString()) : 1;
         public static int CantCopiasPedidoVenta = System.Web.Configuration.WebConfigurationManager.AppSettings["COPIASPEDIDOS"] != null ? int.Parse(System.Web.Configuration.WebConfigurationManager.AppSettings["COPIASPEDIDOS"].ToString()) : 1;
-        public static int CantRenglonesFactura = int.Parse(System.Web.Configuration.WebConfigurationManager.AppSettings["RENGLONESFACTURA"].ToString());
+        //public static int CantRenglonesFactura = int.Parse(System.Web.Configuration.WebConfigurationManager.AppSettings["RENGLONESFACTURA"].ToString());
         public static DateTime MinDatetime = new DateTime(1800, 01, 01);
         public static DateTime MaxDatetime = new DateTime(2200, 01, 01);
         public static Boolean CalculaRetencionesIIBB = System.Web.Configuration.WebConfigurationManager.AppSettings["CALCULARRETENCIONESIIBB"] != null ? Boolean.Parse(System.Web.Configuration.WebConfigurationManager.AppSettings["CALCULARRETENCIONESIIBB"].ToString()) : false;
